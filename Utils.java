a059a97df9a41829f9bcf4dd288f625f9dcb664d
dongzhaoqi <dong18765203770@163.com>
2016-10-29 13:54:15 +0800

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -26,3 +26,42 @@ public static void setPic(ImageView mImageView) {
 }
 
 }
+
+
+
+private Bitmap decodeSampleBitmapFromUri(String absolutePath, int reqWidth, int reqHeight) {  
+        // TODO Auto-generated method stub  
+        Bitmap bm = null;  
+  
+        // First decode with inJustDecodeBounds=true to check dimensions  
+        final BitmapFactory.Options options = new BitmapFactory.Options();  
+        options.inJustDecodeBounds = true;  
+        BitmapFactory.decodeFile(absolutePath, options);  
+  
+        // Calculate inSampleSize  
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);  
+  
+        // Decode bitmap with inSampleSize set  
+        options.inJustDecodeBounds = false;  
+        bm = BitmapFactory.decodeFile(absolutePath, options);  
+        return bm;  
+    }  
+  
+  
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth,  
+                                      int reqHeight) {  
+        // TODO Auto-generated method stub  
+        // Raw height and width of image  
+        final int height = options.outHeight;  
+        final int width = options.outWidth;  
+        int inSampleSize = 1;  
+  
+        if (height > reqHeight || width > reqWidth) {  
+            if (width > height) {  
+                inSampleSize = Math.round((float) height / (float) reqHeight);  
+            } else {  
+                inSampleSize = Math.round((float) width / (float) reqWidth);  
+            }  
+        }  
+        return inSampleSize;  
+    }  
